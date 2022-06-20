7051dd472bef0dea7e52282842855fc81573f2f5
mobiledev101 <mobiledev101@yandex.com>
2022-06-20 12:45:45 -0400

    initial changes

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CleanArchitectureNoteAppTheme {
+
+            }
+        }
+    }
+
+}
+
